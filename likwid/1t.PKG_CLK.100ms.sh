e93b5e4ebf30d330f9347d6bd4806954879abea8
Jimmy Situ <web@jimmystone.cn>
2019-09-20 01:35:21 +0800

    Update CPU GPU mix

--- likwid/1t.PKG_CLK.100ms.sh
+++ likwid/1t.PKG_CLK.100ms.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./likwid-perfscope-ty $1 -g RAPL_PKG_CLK -t 100ms -c 0 likwid-bench -s 10s -t stream_avx -w S0:1MB:1
+./likwid-perfscope-ty -g RAPL_PKG_CLK -t 100ms -c 0 $1 likwid-bench -s 10s -t stream_avx -w S0:1MB:1
--- likwid/4t.PP0vPP1.200ms.sh
+++ likwid/4t.PP0vPP1.200ms.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./likwid-perfscope-xy $1 -g RAPL_PP0_PP1 -t 200ms -c 0 likwid-bench -s 10s -t stream_avx -w S0:1MB:4
+./likwid-perfscope-xy -g RAPL_PP0_PP1 -t 200ms -c 0 $1 likwid-bench -s 100s -t stream_avx -w S0:1MB:4
