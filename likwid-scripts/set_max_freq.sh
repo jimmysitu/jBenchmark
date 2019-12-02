843ebdc6084132e7ab1da9a80f85fb1b0f3b9d21
Jimmy Situ <web@jimmystone.cn>
2019-12-02 19:19:21 +0800

    Update set freq script

--- likwid-scripts/set_max_freq.sh
+++ likwid-scripts/set_max_freq.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-for x in /sys/devices/system/cpu/*/cpufreq/;
-    do echo 1700000 | sudo tee $x/scaling_max_freq; 
+for x in /sys/devices/system/cpu/*/cpufreq/; do
+    echo $1 | sudo tee $x/scaling_max_freq;
 done
+cat /sys/devices/system/cpu/*/cpufreq/scaling_max_freq
