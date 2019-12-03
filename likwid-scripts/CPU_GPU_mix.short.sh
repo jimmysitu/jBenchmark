60e868139d6bda686a090bc5b00aa348cf1b722b
Jimmy Situ <web@jimmystone.cn>
2019-12-03 22:58:52 +0800

    update scope for temperature

---
+++ likwid-scripts/CPU_GPU_mix.short.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd $DIR/../micro-benchmark/OpenDwarfs/build
+
+# Full loading, long runtime
+# ./gemnoui -p x -d 0 -- ../test/n-body-methods/gem/capsid 80 1 0 >/dev/null 2>&1
+# Full loading, short runtime
+# ./gemnoui -p x -d 0 -- ../test/n-body-methods/gem/nucleosome 80 1 0 >/dev/null 2>&1
+# 50% loading, short runtime
+# ./needle -p x -d 0 -- 20480 100 >/dev/null 2>&1
+
+
+# GPU full loading
+for i in {1..1}
+do 
+    for j in {1..4}
+    do
+        ./gemnoui -p 1 -d 0 -- ../test/n-body-methods/gem/nucleosome 80 1 0 >/dev/null 2>&1
+    done
+    # Earn thermal credit
+    echo "GPU full loading interation $i done"
+    sleep 10
+done
+
+# CPU full loading
+for i in {1..1}
+do
+    for j in {1..2}
+    do
+        ./gemnoui -p 0 -d 0 -- ../test/n-body-methods/gem/nucleosome 80 1 0 >/dev/null 2>&1
+    done
+    echo "CPU full loading interation $i done"
+    # Earn thermal credit
+    sleep 5
+done
+
+# GPU half & CPU half
+for i in {1..1}
+do
+	./needle -p 1 -d 0 -- 20480 100 >/dev/null 2>&1 &
+	./needle -p 0 -d 0 -- 20480 100 >/dev/null 2>&1 &
+    wait
+    echo "CPU half & GPU half mix loading interation $i done"
+    sleep 5
+done
+
+# GPU full & CPU half
+for i in {1..1}
+do
+	./gemnoui -p 1 -d 0 -- ../test/n-body-methods/gem/nucleosome 80 1 0 >/dev/null 2>&1 &
+	./needle -p 0 -d 0 -- 20480 100 >/dev/null 2>&1 &
+    wait
+    echo "GPU full & CPU half mix loading interation $i done"
+    # Earn thermal credit
+    sleep 5
+done
+
+# GPU half & CPU full
+for i in {1..1}
+do
+	./needle -p 1 -d 0 -- 20480 100 >/dev/null 2>&1 &
+	./gemnoui -p 0 -d 0 -- ../test/n-body-methods/gem/nucleosome 80 1 0 >/dev/null 2>&1 &
+    wait
+    echo "GPU half & CPU full mix loading interation $i done"
+    # Earn thermal credit
+    sleep 5
+done
+
+# GPU full & CPU full
+for i in {1..1}
+do
+	./gemnoui -p 1 -d 0 -- ../test/n-body-methods/gem/capsid 80 1 0 >/dev/null 2>&1 &
+	./gemnoui -p 0 -d 0 -- ../test/n-body-methods/gem/nucleosome 80 1 0 >/dev/null 2>&1 &
+    wait
+    echo "GPU full & CPU full mix loading interation $i done"
+    # Earn thermal credit
+    sleep 5
+done
+
--- likwid-scripts/set_max_freq.sh
+++ likwid-scripts/set_max_freq.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for x in /sys/devices/system/cpu/*/cpufreq/; do
+for x in /sys/devices/system/cpu/*/cpufreq; do
     echo $1 | sudo tee $x/scaling_max_freq;
 done
 cat /sys/devices/system/cpu/*/cpufreq/scaling_max_freq
